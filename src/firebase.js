afeae3dfcaefdf0e4799cb1745d79e9c62721e50
Lucas Amorim Lima <lucasasmorimlima17@gmail.com>
2022-01-03 16:33:17 -0300

    ininitial stage

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCTghljgS1UxzS6qNxAN2hKZ8foUx6Seno",
+    authDomain: "whatsapp-clone-41a7d.firebaseapp.com",
+    projectId: "whatsapp-clone-41a7d",
+    storageBucket: "whatsapp-clone-41a7d.appspot.com",
+    messagingSenderId: "247701170378",
+    appId: "1:247701170378:web:d2d76aac5377727de0ac33"
+  };
+
+  
