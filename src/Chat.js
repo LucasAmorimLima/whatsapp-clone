04bcf512bc3db348a52a6042ce3ad8c9fe6cc32c
Lucas Amorim Lima <lucasasmorimlima17@gmail.com>
2022-01-03 21:28:35 -0300

    chat

--- src/Chat.js
+++ src/Chat.js
@@ -1,9 +1,62 @@
+import { Avatar, IconButton } from '@material-ui/core'
+import {AttachFile,MoreVert,SearchOutlined,InsertEmoticon,Mic} from '@material-ui/icons'
 import React from 'react'
+
 import "./Chat.css"
 function Chat() {
     return (
         <div className='chat'>
-            <h1>i am the chat</h1>
+            <div className='chat__header'>
+                <Avatar/>
+                <div className='chat__headerInfo'>
+                    <h3>Name</h3>
+                    <p>Last seen at...</p>
+                </div>
+                <div className='chat__headerRigth'>
+                    <IconButton>
+                        <SearchOutlined/>
+                    </IconButton>
+
+                    <IconButton>
+                        <AttachFile/>
+                    </IconButton>
+
+                    <IconButton>
+                        <MoreVert/>
+                    </IconButton>
+                </div>
+            </div>
+            <div className='chat_body'>
+                <p className='chat__message'>
+                <span className='chat__name'>Sonny</span>
+                    This is a message 
+                
+                    <span className='chat__timestamp'>
+                    { new Date().toUTCString()}
+                    </span>
+
+                </p>
+
+                <p className='chat__message chat__reciever'>
+                <span className='chat__name'>Sonny</span>
+                    This is a message 
+                
+                    <span className='chat__timestamp'>
+                    { new Date().toUTCString()}
+                    </span>
+
+                </p>
+            </div>
+            <div className='chat__footer'>
+                <InsertEmoticon/>
+                <form>
+                    <input placeholder='Type a message' type="text"/>
+                    <button type='submit'>
+                        Send a message
+                    </button>
+                </form>
+                <Mic/>
+            </div>
         </div>
     )
 }
